f8abc3fc5925055b6d1b99de297f3c7d0fd52010
Felix Ngetich <fellixkn@gmail.com>
2023-03-24 12:32:11 -0700

    fix

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,16 +1,4 @@
 #include "main.h"
-#include "main.h"
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-
-int main(void)
-{
-    print_numbers();
-    return (0);
 
 /**
  * print_numbers - print 0-9
