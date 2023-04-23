949c53177720e46d23a90f7f4b1d9675939363d5
Felix Ngetich <fellixkn@gmail.com>
2023-04-23 08:08:07 -0700

    fix

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -9,11 +9,17 @@
 
 int _sqrt_recursion(int n)
 {
+	int i = 0;
+
 	if (n < 0)
 		return (-1);
 	if (n == 0)
 		return (0);
 	if (n == 1)
 		return (1);
+	if (i * i > n)
+		return (-1);
+	if (i * i == n)
+		return (i);
 	return (_sqrt_recursion(n));
 }
