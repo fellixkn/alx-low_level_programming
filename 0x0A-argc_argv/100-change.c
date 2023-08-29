fb7034f6000c46f49f6fa9a3169d0024cf7da631
fellixkn <fellixkn@gmail.com>
2023-08-28 21:31:29 -0700

    correction

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -6,7 +6,7 @@
  * main - prints the minimum number of coins to
  * make change for an amount of money
  * @argc: number of arguments
-  * @argv: array of arguments
+ * @argv: array of arguments
  *
  * Return: 0 (Success), 1 (Error)
  */
